6857ae32145bd6cd24e4817971c7a2fdd01ade48
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-01 15:12:29 +1200

    Updated construction page to have a background colour during loading to reduce harshness of fade-in

--- static/JavaScript/background_image_load.js
+++ static/JavaScript/background_image_load.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var image_num = Math.floor((Math.random() * 7) + 1);
     var background = new Image();
     background.src = "/static/Media/Photographs/Backgrounds/" + image_num + ".jpg";
+
+    /*Show background color AND hide the background image until loading*/
     $(".under-construction-background").hide();
-    $(background).on('load', function() {
-        $(".under-construction-background").css({"background-image" : "url(" + $(this).attr("src") + ")",
+    $(background).on('load', function () {
+        $(".under-construction-background").css({
+            "background-image": "url(" + $(this).attr("src") + ")",
             "background-position": "center",
             "background-repeat": "no-repeat",
-            "background-size": "cover"}).fadeIn(3000)
+            "background-size": "cover"
+        }).fadeIn(1000, function () {
+            /*Remove temporary background colour*/
+            $(".under-construction-background-temp").remove();
+
+        });
 
     });
 });
