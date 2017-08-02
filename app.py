45991c97b901f0be869a0d53cba1c382835dd95e
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-02 12:29:17 +1200

    initial commit 02082017

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import Flask, render_template, flash, redirect, request, session, url_for
 from flask_session import Session
+from tempfile import mkdtemp
 
 from passlib.hash import sha256_crypt
 
@@ -10,7 +11,14 @@ import os
 
 from mysql_db import MySQL_Database
 
+# Configure application
 app = Flask(__name__)
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route('/')
@@ -21,12 +29,49 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (i.e. login form submission)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html", err_message="Please provide a username")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html", err_message="Please provide your password")
+
+    #     # query database for username
+    #     rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+    #
+    #     # ensure username exists and password is correct
+    #     if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+    #         return apology("invalid username and/or password")
+    #
+    #     # remember which user has logged in
+    #     session["user_id"] = rows[0]["id"]
+    #
+    #     # redirect user to home page
+    #     return redirect(url_for("index"))
+    #
+    # # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 
 @app.route('/logout')
 def logout():
-    return render_template('under_construction.html')
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("login"))
 
 
 @app.route('/test')
