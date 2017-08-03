3d772034ce66f5141b9998705b1c43d1ce01b6b8
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-03 15:26:07 +1200

    tweaking sessions

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ if __name__ == '__main__':
         local = config['LOCAL']
         app.secret_key = local['SECRET_KEY']
     else:
+        print("test")
         app.secret_key = os.environ['SECRET_KEY']
 
     app.run()
