d1de2d72c26b027980a58f52695ea1bec0080d24
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-03 16:12:05 +1200

    attempting to setup redis server for session storage

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, flash, redirect, request, session, url
 from flask_session import Session
 from tempfile import mkdtemp
 import os
+from redissession import *
 from mysql_db import MySQL_Database
 from passlib.hash import sha256_crypt
 
@@ -9,6 +10,7 @@ from helpers import *
 
 # Configure application
 app = Flask(__name__)
+redis = Redis(app)
 
 if os.environ['ENV_TYPE'] == 'LOCAL':
     import configparser
@@ -23,11 +25,10 @@ if os.environ['ENV_TYPE'] == 'LOCAL':
 else:
     app.secret_key = os.environ['SECRET_KEY']
 
-# configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# configure session to use Redis (instead of signed cookies or local storage. Local storage was causing issues amongst multiple workers used for gunicorn server)
+app.config["SESSION_TYPE"] = "redis"
+app.session_interface = RedisSessionInterface()
+
 
 @app.route('/')
 @login_required
