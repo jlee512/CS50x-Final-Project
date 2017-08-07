f7228654be01123dd531a8348bf6e2964183f70f
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-07 22:33:15 +1200

    Basic password strength checker implemented

---
+++ static/JavaScript/walk_card_feed.js
@@ -0,0 +1,133 @@
+/*-------------------------------------------------------*/
+/*This JavaScript file is used to display 'Great Walks' cards on the main content page, this is to be done via infinite scrollingy*/
+/*-------------------------------------------------------*/
+
+{#Waikaremoana#}
+var great_walks = [{walk_id: 1, walk_name: "Lake Waikaremoana", class_name: "waikare-walk card"}];
+
+var walk_card_template = '<div class="card">' +
+                            <h2 class="card-title-text">
+                            <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                        </h2>
+                    </div>
+
+                    {#Tongariro Northern Circuit#}
+                    <div class="tongariro-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Tongariro Northern Circuit
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Whanganui River Journey#}
+                    <div class="whanganui-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Whanganui River Journey
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Abel Tasman Coastal Track#}
+                    <div class="abel-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Abel Tasman Coastal Track
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Heaphy Tasman Coastal Track#}
+                    <div class="heaphy-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Heaphy Track
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Paparoa Track#}
+                    <div class="paparoa-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Paparoa + Pike 29 Memorial Track (2019)
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Milford Track#}
+                    <div class="milford-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Milford Track
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Routeburn Track#}
+                    <div class="routeburn-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Routeburn Track
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Kepler Track#}
+                    <div class="kepler-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Kepler Track
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    {#Rakiura Track#}
+                    <div class="rakiura-walk card">
+                        <div class="card-title">
+                            <h2 class="card-title-text">Rakiura Track
+                                <span class="card-icon">
+                            <button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                              <i class="material-icons walk-card-icon">directions_walk</i>
+                            </button>
+                        </span>
+                            </h2>
+                        </div>
+                    </div>
