f69855243c11a68ca13adf36660c2d57fea8617c
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-08 16:22:18 +1200

    walk_card_feed loading tweaked and progressed

--- static/JavaScript/walk_card_feed.js
+++ static/JavaScript/walk_card_feed.js
@@ -3,26 +3,66 @@
 /*-------------------------------------------------------*/
 
 // Great Walks JSON Array
-var great_walks = [{walk_id: 1, walk_name: "Lake Waikaremoana", class_name: "waikare-walk"},
-    {walk_id: 2, walk_name: "Tongariro Northern Circuit", class_name: "tongariro-walk"},
-    {walk_id: 3, walk_name: "Whanganui River Journey", class_name: "whanganui-walk"},
-    {walk_id: 4, walk_name: "Abel Tasman Coastal Track", class_name: "abel-walk"},
-    {walk_id: 5, walk_name: "Heaphy Track", class_name: "heaphy-walk"},
-    {walk_id: 6, walk_name: "Paparoa + Pike 29 Memorial Track (2019)", class_name: "paparoa-walk"},
-    {walk_id: 7, walk_name: "Milford Track", class_name: "milford-walk"},
-    {walk_id: 8, walk_name: "Routeburn Track", class_name: "routeburn-walk"},
-    {walk_id: 9, walk_name: "Kepler Track", class_name: "kepler-walk"},
-    {walk_id: 10, walk_name: "Rakiura Track", class_name: "rakiura-walk"}];
-
-var walk_card_template = '<div class="card">'
-    + '<h2 class="card-title-text">'
-    + '<span class="card-icon">'
-    + '<button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">'
-    + '<i class="material-icons walk-card-icon">directions_walk</i>'
-    + '</button>'
-    + '</span>'
-    + '</h2>'
-    + '</div>';
+var great_walks = [{
+    walk_id: 1,
+    walk_name: "Lake Waikaremoana",
+    class_name: "waikare-walk",
+    background_image: "/static/Media/Photographs/Walks/lake-waikaremoana.jpg"
+},
+    {
+        walk_id: 2,
+        walk_name: "Tongariro Northern Circuit",
+        class_name: "tongariro-walk",
+        background_image: "/static/Media/Photographs/Walks/tongariro_northern_circuit.jpg"
+    },
+    {
+        walk_id: 3,
+        walk_name: "Whanganui River Journey",
+        class_name: "whanganui-walk",
+        background_image: "/static/Media/Photographs/Walks/whanganui-river-journey.jpg"
+    },
+    {
+        walk_id: 4,
+        walk_name: "Abel Tasman Coastal Track",
+        class_name: "abel-walk",
+        background_image: "/static/Media/Photographs/Walks/abel-tasman.jpg"
+    },
+    {
+        walk_id: 5,
+        walk_name: "Heaphy Track",
+        class_name: "heaphy-walk",
+        background_image: "/static/Media/Photographs/Walks/heaphy-track.jpg"
+    },
+    {
+        walk_id: 6,
+        walk_name: "Paparoa + Pike 29 Memorial Track (2019)",
+        class_name: "paparoa-walk",
+        background_image: "/static/Media/Photographs/Walks/paparoa-track.jpg"
+    },
+    {
+        walk_id: 7,
+        walk_name: "Milford Track",
+        class_name: "milford-walk",
+        background_image: "/static/Media/Photographs/Walks/milford-track.jpg"
+    },
+    {
+        walk_id: 8,
+        walk_name: "Routeburn Track",
+        class_name: "routeburn-walk",
+        background_image: "/static/Media/Photographs/Walks/kepler-track.jpg"
+    },
+    {
+        walk_id: 9,
+        walk_name: "Kepler Track",
+        class_name: "kepler-walk",
+        background_image: "/static/Media/Photographs/Walks/kepler-track.jpg"
+    },
+    {
+        walk_id: 10,
+        walk_name: "Rakiura Track",
+        class_name: "rakiura-walk",
+        background_image: "/static/Media/Photographs/Walks/routeburn track.jpg"
+    }];
 
 //Setup global variables to store the state of walk-feed loading on the page at a given point in time (searching/sorting mechanisms to be factored in at a later stage)
 var from = 0;
@@ -53,16 +93,6 @@ $(document).ready(function () {
         main_scroll = $('main').scrollTop();
         main_height = $('main').height();
         window_height = $(window).height();
-
-        console.log("Scroll: " + main_scroll);
-        console.log("Window: " + window_height);
-        console.log("Main " + main_height);
-        console.log("Section 1 " + $('.mdl-layout__header-row').height());
-        console.log("Section 2: " + $('.mdl-layout__tab.is-active').height());
-        console.log("Section 3: " + $('#map').height());
-        console.log("Walk card feed: " + $('.walk-card-feed').height());
-        console.log("More walks: " + more_walks);
-        console.log("scroll reg: " + scroll_registered);
 
         if ((main_height - 10) <= (window_height + main_scroll) && more_walks && !scroll_registered) {
             console.log("scroll registered");
@@ -102,9 +132,6 @@ function load_walks_increment() {
 /*If a successful AJAX call is made, this function is called to process the results and populate the walk_card_template and subsequently the walk-card-feed.*/
 function successful_walks_load(data) {
 
-    //Access the card container where loaded cards are to be appended
-    var walk_card_insert_location = $('#loader');
-
     if (data.length == 0) {
         //    If the data length is zero, there are no more walks to load
         $('.walk-feed-loader').hide();
@@ -112,27 +139,69 @@ function successful_walks_load(data) {
         more_walks = false;
 
     } else {
+
+        var walk_cards = [];
+
         for (var i = 0; i < data.length; i++) {
             var individual_walk = data[i];
-            var walk_template_to_populate = $(walk_card_template);
 
-            //    Add href attribute at a later stage to link to more detailed walk information
-            walk_template_to_populate.addClass(individual_walk.class_name);
-            walk_template_to_populate.find('.card-title-text').prepend(individual_walk.walk_name);
+            //Transfer individual walk data to Walk_Card object instance
+            walk_cards[i] = new Walk_Card(individual_walk.walk_id, individual_walk.walk_name, individual_walk.class_name, individual_walk.background_image);
 
-            //Once the content has been loaded, hide the loader and append the walk card to the walk-card-feed
-            $('.walk-feed-loader').hide();
-            walk_template_to_populate.insertBefore(walk_card_insert_location);
-
-            //    If the message length is less than the requested count, hide the loader and set 'more_walks' to false as the end of the walks list has been reached
-            if (data.length < count) {
-                $('.walk-feed-loader').hide();
-                $('#loaded1, #loaded2, #loaded3, #loaded4').show();
-                more_walks = false;
-
-            }
         }
+
+        for (var i = 0; i < walk_cards.length; i++) {
+            console.log("test");
+            walk_cards[i].load();
+
+        }
+
+//         background: white; /* For browsers that do not support gradients */
+// background: -webkit-linear-gradient(right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4)), url('/static/Media/Photographs/Walks/abel-tasman.jpg') center / cover; /*Safari 5.1-6*/
+// background: -o-linear-gradient(left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4)), url('/static/Media/Photographs/Walks/abel-tasman.jpg') center / cover; /*Opera 11.1-12*/
+// background: -moz-linear-gradient(left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4)), url('/static/Media/Photographs/Walks/abel-tasman.jpg') center / cover; /*Fx 3.6-15*/
+// background: linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4)), url('/static/Media/Photographs/Walks/abel-tasman.jpg') center / cover; /*Standard*/
     }
-//    Once the previous AJAX call has been completed, allow another scroll-bottom event to be registered
+    $('.walk-feed-loader').hide();
+    //    Once the previous AJAX call has been completed, allow another scroll-bottom event to be registered
     scroll_registered = false;
+}
+
+function Walk_Card(walk_id, walk_name, class_name, background_image) {
+    //Private instance variables (instance specific)
+    this.walk_id = walk_id;
+    this.walk_name = walk_name;
+    this.class_name = class_name;
+    this.background_image = background_image;
+    this.insert_location = $('#loader');
+
+    //Priviate variable (class specific)
+    this.walk_card_template = $('<div class="card ' + this.class_name + '">'
+        + '<h2 class="card-title-text">'
+        + this.walk_name
+        + '<span class="card-icon">'
+        + '<button class="mdl-button mdl-button--icon mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">'
+        + '<i class="material-icons walk-card-icon">directions_walk</i>'
+        + '</button>'
+        + '</span>'
+        + '</h2>'
+        + '</div>');
+
+    this.load = function () {
+        var bg_img = new Image();
+        bg_img.src = this.background_image;
+        var walk_card_template = this.walk_card_template;
+        var background_image = this.background_image;
+
+        $(bg_img).on('load', function () {
+            
+            walk_card_template.find('h2.card-title-text').css('background', '-webkit-linear-gradient(right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4)), url(\'' + background_image + '\') center / cover');
+
+            //Once the content has been loaded, append the walk card to the walk-card-feed\
+            walk_card_template.insertBefore('#loader');
+
+        });
+
+    }
+
 }
