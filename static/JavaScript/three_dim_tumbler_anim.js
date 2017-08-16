aba93ff70f4ad5238d6a688b6c0de35899c533a5
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-16 16:27:15 +1200

    Basic tumbler implementation completed

---
+++ static/JavaScript/three_dim_tumbler_anim.js
@@ -0,0 +1,40 @@
+//Initiate tumbler animation once the document is ready
+$(document).ready(function () {
+    //Developed by J. Lees, JS-Fiddle: https://jsfiddle.net/2Tokoeka/nkws2dfz/
+
+    var timer = setInterval(item_transition, 4000);
+    var hide_item_num = 1;
+    var show_item_num = 2;
+    var num_items = 2;
+
+
+    function item_transition() {
+        var item_id = "tumble-";
+        var item_hide_id = item_id + hide_item_num;
+        var item_show_id = item_id + show_item_num;
+
+        var hide_element = $('#' + item_hide_id);
+        var show_element = $('#' + item_show_id);
+
+        hide_element.on('animationend webkitAnimationEnd oAnimationEnd', function () {
+            hide_element.removeClass('rotate-out');
+            hide_element.removeClass('tumble-active');
+
+        });
+
+        show_element.on('animationend webkitAnimationEnd oAnimationEnd', function () {
+            show_element.removeClass('rotate-in');
+            show_element.addClass('tumble-active');
+        });
+
+        hide_element.addClass('rotate-out');
+        show_element.addClass('rotate-in');
+
+        hide_item_num = show_item_num;
+        show_item_num++;
+        if (show_item_num > num_items) {
+            show_item_num = 1;
+        }
+    }
+
+});
